3711ca94c735da51d4c1cf0ba1650949955ee80d
zakhodiaeva.margarita <margarita.zakhodiaeva@devexpress.com>
2020-06-15 21:04:42 +0300

    add CS solution

---
+++ CS/WinDesignerDashboardState/DesignerForm1.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWin;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace WinDesignerDashboardState
+{
+    public partial class DesignerForm1: DevExpress.XtraBars.Ribbon.RibbonForm {
+        const string path = @"..\..\Dashboards\dashboardWithSavedState.xml";
+        public DesignerForm1() {
+            InitializeComponent();
+            dashboardDesigner.CreateRibbon();
+            dashboardDesigner.UseNeutralFilterMode = true;
+            dashboardDesigner.LoadDashboard(path);
+        }
+
+        public DashboardState CreateDashboardState() {
+            DashboardState state = new DashboardState();
+            //Clears selections in the LisxBox item.
+            state.Items.Add(new DashboardItemState("listBoxDashboardItem1") {
+                MasterFilterValues = new List<object[]>()
+            });
+            //The lines below clears all values if neutral filter mode is disabled:
+            //state.Items.Add(new DashboardItemState("listBoxDashboardItem1") {
+            //    MasterFilterValues = null
+            //});
+            return state;
+        }
+        private void dashboardDesigner_CustomizeDashboardTitle(object sender, DevExpress.DashboardWin.CustomizeDashboardTitleEventArgs e) {
+            DashboardToolbarItem resetStateItem = new DashboardToolbarItem("Reset State",
+              new Action<DashboardToolbarItemClickEventArgs>((args) => {
+                  dashboardDesigner.SetDashboardState(CreateDashboardState());
+              }));
+            resetStateItem.Caption = "Reset Dashboard State";
+            e.Items.Add(resetStateItem);
+        }
+    }
+}
